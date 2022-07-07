69ef4e3b66cb88c5f26fbb50e77f4831668dcaec
William Lee <jacoblee21@live.com>
2022-07-07 18:56:11 -0500

    fix some stuff

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
 export { default as Inventory } from "./inventory/Inventory";
 export { default as Manage } from "./manage/Manage";
 export { default as MainPage } from "./mainpage/MainPage";
-export { default as Sign } from "./sign/Sign";
+export { SignIn } from "./sign/Sign";
